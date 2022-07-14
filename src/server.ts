ee228fd4bb1ce792578ef3f5b96981e576943b4b
Mizael <mizaellmagumba@gmail.com>
2022-07-14 22:56:33 +0200

    added prisma ORM

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,46 @@
 import express from 'express'
 import { prisma } from './prisma';
+import nodemailer from 'nodemailer'
+import { type } from 'os';
 
 const app = express();
 app.use(express.json())
 
-app.post('/feedbacks', (req, res) => {
- prisma.feedbacks.create({
-  
- })
-  
+const transport = nodemailer.createTransport({
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: "3c6149fd661c40",
+    pass: "2f5ca846c152af"
+  }
+});
 
-  return res.send('Hello World')
-})
+app.post('/feedbacks', async (req, res) => {
+
+  const { type, comment, screanshot } = req.body
+  const feedback = await prisma.feedback.create({
+    data: {
+      type,
+      comment,
+      screanshot,
+    }
+  })
+
+  await transport.sendMail({
+    from: 'Equipe Feedget <oi@feedget.com>',
+    to: 'Mizael Fernando <mizaellmagumba@gmail.com',
+    subject: "Novo Feedback",
+    html: [
+      `<div style="font-family: sans-serif; font-size: 16px; color: #111;" >`,
+      `<p>Tipo de Feedback: ${type}</p>`,
+      `<p>Comentário: ${comment} </p>`,
+      `</div>`,
+    ].join('\n')
+  })
+
+  return res.status(201).json({data: feedback})
+});
 
 app.listen(3333, () => {
-  console.log('Server ')
+  console.log('Server Running ')
 })
