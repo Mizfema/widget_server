79023e42c717728fc1a27e30a3598b65d72343ca
Mizael <mizaellmagumba@gmail.com>
2022-08-03 22:50:46 +0200

    Erro de deploy 404

--- src/routes.ts
+++ src/routes.ts
@@ -11,35 +11,26 @@ export const routes = express.Router();
 routes.post('/feedbacks', async (req, res) => {
 
   const { type, comment, screanshot } = req.body
- 
-  const prismaFeedbacksRepository = new PrismaFeedbacksRepository();
-  const nodemailerMailAdapter = new NodemailerMailAdapter()
+
+  try {
+    const prismaFeedbacksRepository = new PrismaFeedbacksRepository();
+    const nodemailerMailAdapter = new NodemailerMailAdapter()
 
 
-  const feedbackUseCase = new FeedbackUseCase(
-    prismaFeedbacksRepository,
-    nodemailerMailAdapter
+    const feedbackUseCase = new FeedbackUseCase(
+      prismaFeedbacksRepository,
+      nodemailerMailAdapter
     );
 
-  await feedbackUseCase.execute({
-    type,
-    comment,
-    screanshot,
-  })
+    await feedbackUseCase.execute({
+      type,
+      comment,
+      screanshot,
+    });
 
-
-
-// await transport.sendMail({
-//   from: 'Equipe Feedget <oi@feedget.com>',
-//   to: 'Mizael Fernando <mizaellmagumba@gmail.com',
-//   subject: "Novo Feedback",
-//   html: [
-//     `<div style="font-family: sans-serif; font-size: 16px; color: #111;" >`,
-//     `<p>Tipo de Feedback: ${type}</p>`,
-//     `<p>Comentário: ${comment} </p>`,
-//     `</div>`,
-//   ].join('\n')
-// })
-
-  return res.status(201).send();
+    return res.status(201).send();
+  } catch (error) {
+    
+    return res.status(500).send();
+  }
 });
